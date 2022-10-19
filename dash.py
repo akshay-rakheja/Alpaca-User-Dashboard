a6c6b43018fea439535a60206f78dfca603aaa7b
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-10-19 00:31:07 -0400

    adding gitignore

--- dash.py
+++ dash.py
@@ -7,11 +7,13 @@ import streamlit as st
 # no keys required for crypto data
 client = CryptoHistoricalDataClient()
 
-request_params = CryptoBarsRequest(
-    symbol_or_symbols=["BTC/USD", "ETH/USD"],
-    timeframe=TimeFrame.Day,
-    start="2022-07-01"
-)
+# request_params = CryptoBarsRequest(
+#     symbol_or_symbols=["BTC/USD", "ETH/USD"],
+#     timeframe=TimeFrame.Day,
+#     start="2022-07-01"
+# )
+
+st.header("Alpaca Trading Dashboard")
 
 
 # bars = client.get_crypto_bars(request_params)
